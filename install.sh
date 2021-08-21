2e8c0f2c89cefc14c71ba2e8176e1087737d491e
David C <datcrosb@ucsc.edu>
2021-08-21 13:46:28 -0700

    hi

--- install.sh
+++ install.sh
@@ -2,8 +2,3 @@
 ln -sf ~/dotfiles/vimrc ~/.vimrc
 ln -sf ~/dotfiles/bashrc ~/.bashrc
 
-# Vim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
-cd ~/.vim/bundle/YouCompleteMe
-python3 install.py --clang-completer
---
+++ vundle.sh
@@ -0,0 +1,5 @@
+# Vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+cd ~/.vim/bundle/YouCompleteMe
+python3 install.py --clang-completer
